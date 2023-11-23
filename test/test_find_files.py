5ce8ec74a431cf775d7ecd8c0c833c9a310d9cc6
adrianB1996 <rianbourke@gmail.com>
2023-11-23 20:18:23 +0000

    Sorting import errors experienced with pytest.

--- test/test_find_files.py
+++ test/test_find_files.py
@@ -2,8 +2,25 @@ import os
 
 import pytest
 
-from bin.workflow import find_files
+import glob
 
+
+
+def find_files(folder_path, expression):
+    """
+    Finds all files in the specified folder that match the given expression.
+
+    Args:
+        folder_path (str): The path to the folder to search.
+        expression (str): A glob expression to match file names.
+
+    Returns:
+        list[str]: A list of file paths.
+    """
+
+    files = glob.glob(os.path.join(folder_path, expression))
+
+    return files
 
 def test_find_files():
     # Create temporary directory for testing
